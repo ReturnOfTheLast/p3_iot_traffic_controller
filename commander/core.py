9c930c92eea8482f8c73151452bf738f7c769782
returnofthelast <ReturnOfTheLast@gmail.com>
2023-11-02 12:01:14 +0100

    Made the commander add a jump to our chain

--- commander/core.py
+++ commander/core.py
@@ -3,13 +3,25 @@
 
 from queuemanager.core import CommandQueue
 from logger import LoggingObject
+from iptc import Chain, Table, Target, Rule
 
 
 class Commander(LoggingObject):
     def __init__(self, command_queue: CommandQueue):
         self.command_queue: CommandQueue = command_queue
         self.logger.info("Commander initiating...")
-        # TODO: Add custom chain to IPTables
+
+        self.logger.info("Creating custom chain")
+        self.table = Table(Table.FILTER)
+        self.logger.debug(f"Table: \n{self.table}")
+        self.sec_chain = self.table.create_chain("SEC-CHAIN")
+        self.logger.debug(f"Chain: \n{self.sec_chain}")
+        self.logger.info("Making the FORWARD chain check our chain first")
+        jump_rule = Rule()
+        sec_chain_target = Target(jump_rule, self.sec_chain.name)
+        jump_rule.target = sec_chain_target
+        forward_chain = Chain(self.table, "FORWARD")
+        forward_chain.insert_rule(jump_rule)
 
     def run(self):
         while True:
