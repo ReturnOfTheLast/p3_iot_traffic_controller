25db9c8a537a0092028acb33e00033c0c5d9f55a
returnofthelast <ReturnOfTheLast@gmail.com>
2023-11-03 10:08:42 +0100

    Hope this adds our chain

--- commander/core.py
+++ commander/core.py
@@ -3,14 +3,19 @@
 
 from queuemanager.core import CommandQueue
 from logger import LoggingObject
+from nftables import Nftables
+import json
 
 
 class Commander(LoggingObject):
     def __init__(self, command_queue: CommandQueue):
         self.command_queue: CommandQueue = command_queue
         self.logger.info("Commander initiating...")
-
-        self.logger.info("Creating custom chain")
+        self.nft = Nftables()
+        self.nft.set_json_output(True)
+        self.chain_name = "SEC-CHAIN"
+        self.nft.cmd(f"add chain inet filter {self.chain_name}")
+        self.nft.cmd(f"insert rule filter FORWARD 0 jump {self.chain_name}")
 
     def run(self):
         while True:
