eb1e8bb2c16fbdc7dc5d0c6a2ca98236d2fe8393
returnofthelast <ReturnOfTheLast@gmail.com>
2023-11-03 08:49:21 +0100

    Made the Formatters singletons

--- logger/__init__.py
+++ logger/__init__.py
@@ -15,30 +15,36 @@ from abc import ABC
 from singleton import Singleton
 
 
-class CustomFormatter(Formatter):
-    def __init__(self, time=False):
+class FileFormatter(Formatter, metaclass=Singleton):
+    def __init__(self):
         lformat: str = "{asctime:<25s} {name:<35s} {levelname:>8s}: {message}"
+        Formatter.__init__(self, lformat, style="{")
+
+
+class ConsoleFormatter(Formatter, metaclass=Singleton):
+    def __init__(self):
+        lformat: str = "{name:<35s} {levelname:>8s}: {message}"
         Formatter.__init__(self, lformat, style="{")
 
 
 class DebugFileHandler(FileHandler, metaclass=Singleton):
     def __init__(self):
         FileHandler.__init__(self, "debug.log")
-        self.setFormatter(CustomFormatter(time=True))
+        self.setFormatter(FileFormatter())
         self.setLevel(DEBUG)
 
 
 class InfoFileHandler(FileHandler, metaclass=Singleton):
     def __init__(self):
         FileHandler.__init__(self, "info.log")
-        self.setFormatter(CustomFormatter(time=True))
+        self.setFormatter(FileFormatter())
         self.setLevel(INFO)
 
 
 class ConsoleHandler(StreamHandler, metaclass=Singleton):
     def __init__(self):
         StreamHandler.__init__(self, sys.stdout)
-        self.setFormatter(CustomFormatter())
+        self.setFormatter(ConsoleFormatter())
         self.setLevel(INFO)
 
 
