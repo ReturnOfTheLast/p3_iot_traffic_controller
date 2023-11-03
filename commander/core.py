73e2cbc89d4e9b4583d18a99aae7af3ea0b21491
returnofthelast <ReturnOfTheLast@gmail.com>
2023-11-03 10:56:04 +0100

    Made it use json (and hopefully work)

--- commander/core.py
+++ commander/core.py
@@ -14,8 +14,45 @@ class Commander(LoggingObject):
         self.nft = Nftables()
         self.nft.set_json_output(True)
         self.chain_name = "SEC-CHAIN"
-        self.nft.cmd(f"add chain inet filter {self.chain_name}")
-        self.nft.cmd(f"insert rule filter FORWARD 0 jump {self.chain_name}")
+        nft_command = {
+            "nftables": [
+                {
+                    "add": {
+                        "chain": {
+                            "table": "filter",
+                            "name": "SEC-CHAIN"
+                        }
+                    }
+                },
+                {
+                    "insert": {
+                        "rule": {
+                            "table": "filter",
+                            "chain": "FORWARD",
+                            "index": 0,
+                            "expr": [
+                                {
+                                    "jump": {
+                                        "target": "SEC-CHAIN"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            ]
+        }
+        self.logger.info("Setting up our chain and rules on NFTables...")
+        self.logger.debug(
+            f"NFTables command: \n{json.dumps(nft_command, indent=2)}")
+        rc, output, error = self.nft.json(nft_command)
+        self.logger.debug(f"Return code: {rc}")
+        self.logger.debug(f"Output: \n{json.dumps(output, indent=2)}")
+        self.logger.debug(f"Error: \n{json.dumps(error, indent=2)}")
+        if rc != 0:
+            self.logger.info("NFTables command failed (see debug.log)")
+            exit(1)
+        self.logger.info("Configured NFTables")
 
     def run(self):
         while True:
